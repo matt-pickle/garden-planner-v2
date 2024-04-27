6ba2dcf88aaf805b0caea2cca7c90b0a09a967a1
Matt <mpickle86@gmail.com>
2024-04-27 14:13:47 -0500

    Make login stylesheet dynamic

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,11 @@
 import { Text, View, Pressable, Image } from "react-native"
 import { logIn } from "../firebase/firebase-auth-methods"
 import { FontAwesome5 } from '@expo/vector-icons';
-import styles from "../styles/LoginStyles.js"
+import createStyleSheet from "../styles/LoginStyles.js"
 
 export default function Login() {
+  const styles = createStyleSheet()
+
   return (
     <View style={styles.loginScreen}>
       <Image style={styles.loginScreenLogo} source={require("../assets/logo.png")} />
--- styles/LoginStyles.js
+++ styles/LoginStyles.js
@@ -1,37 +1,43 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet, Dimensions } from "react-native"
 import { cream } from "./style-variables.js"
 
-const styles = StyleSheet.create({
-  loginScreen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  loginScreenLogo: {
-    height: 210,
-    width: 300,
-    resizeMode: "contain",
-    marginBottom: 100
-  },
-  loginButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderColor: cream,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderRadius: 8,
-    paddingVertical: 10,
-    paddingHorizontal: 20
-  },
-  loginButtonText: {
-    color: cream,
-    fontSize: 16
-  },
-  googleIcon: {
-    color: cream,
-    fontSize: 24,
-    marginRight: 15
+export default function createStyleSheet() {
+  const dim = Dimensions.get("screen")
+  let logoMarginBottom = 100
+  if (dim.width > dim.height) {
+    logoMarginBottom = 25
   }
-})
 
-export default styles
+  return StyleSheet.create({
+    loginScreen: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    loginScreenLogo: {
+      height: 210,
+      width: 300,
+      resizeMode: "contain",
+      marginBottom: logoMarginBottom
+    },
+    loginButton: {
+      flexDirection: "row",
+      alignItems: "center",
+      borderColor: cream,
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderRadius: 8,
+      paddingVertical: 10,
+      paddingHorizontal: 20
+    },
+    loginButtonText: {
+      color: cream,
+      fontSize: 16
+    },
+    googleIcon: {
+      color: cream,
+      fontSize: 24,
+      marginRight: 15
+    }
+  })
+}

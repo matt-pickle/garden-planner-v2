fc80fd7689c85997da3d155eaa9addea1e5dcd0c
Matt <mpickle86@gmail.com>
2024-05-04 19:45:28 -0500

    Add CreateGardenModal

--- components/CreateGardenModal.js
+++ components/CreateGardenModal.js
@@ -1,11 +1,50 @@
 import { useState } from "react"
-import { View, Text } from "react-native"
-// import styles from "../styles/styles.js"
+import { Modal, Text, View, Pressable, TextInput } from "react-native"
+import styles from "../styles/CreateGardenModalStyles"
 
-export default function CreateGardenModal() {
+export default function CreateGardenModal({ createNewGarden, isCreateGardenModalVisible, setIsCreateGardenModalVisible }) {
+  const [gardenName, setGardenName] = useState("");
+
+  function handleOK() {
+    createNewGarden(gardenName);
+    setGardenName("");
+  }
+
+  function handleCancel() {
+    setIsCreateGardenModalVisible(false);
+    setGardenName("");
+  }
+
   return (
-    <View>
-      <Text>CreateGardenModal</Text>
-    </View>
+    <Modal
+      animationType="slide"
+      visible={isCreateGardenModalVisible}
+      transparent={true}
+      onRequestClose={() => props.setIsCreateGardenModalVisible(false)}
+    >
+      <View style={[styles.modal, styles.centeredModal]}>
+        <Text style={styles.modalHeader}>CREATE NEW GARDEN</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholderTextColor="rgb(120,120,130)"
+          placeholder="Garden Name"
+          maxLength={20}
+          value={gardenName}
+          onChangeText={text => setGardenName(text)}
+        />
+        <View style={styles.modalButtonRow}>
+          <Pressable
+            onPress={handleOK}
+          >
+            <Text style={styles.modalButton}>OK</Text>
+          </Pressable>
+          <Pressable
+            onPress={handleCancel}
+          >
+            <Text style={styles.modalButton}>CANCEL</Text>
+          </Pressable>
+        </View>        
+      </View>
+    </Modal>
   )
 }
---
+++ styles/CreateGardenModalStyles.js
@@ -0,0 +1,116 @@
+import { StyleSheet } from "react-native"
+import { lightFont, boldFont, cream, darkCream, darkGreen } from "./style-variables.js"
+
+const styles = StyleSheet.create({
+  modal: {
+    flexDirection: "column",
+    alignSelf: "center",
+    alignItems: "flex-start",
+    backgroundColor: cream,
+    borderColor: darkCream,
+    borderWidth: 3,
+    borderRadius: 5,
+    width: 300,
+    padding: 15,
+    marginTop: 25,
+    elevation: 5
+  },
+
+  centeredModal: {
+    alignItems: "center"
+  },
+
+  modalTopRowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%"
+  },
+
+  modalHeader: {
+    color: darkGreen,
+    fontFamily: boldFont,
+    fontSize: 18,
+    marginBottom: 15
+  },
+
+  modalX: {
+    color: darkGreen,
+    fontFamily: boldFont,
+    fontSize: 24,
+  },
+
+  modalXButton: {
+    marginTop: -15,
+    marginRight: -5
+  },
+
+  modalText: {
+    color: darkGreen,
+    fontFamily: lightFont,
+    fontSize: 16,
+    marginBottom: 15
+  },
+
+  modalPickerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: cream,
+    borderWidth: 2,
+    borderColor: darkCream,
+    borderRadius: 5,
+    paddingVertical: 5,
+    marginTop: -10,
+    marginBottom: 5
+  },
+
+  modalPicker: {
+    color: darkGreen,
+    width: 90,
+    marginRight: -5
+  },
+
+  modalInput: {
+    textAlign: "center",
+    width: 230,
+    color: darkGreen,
+    fontFamily: lightFont,
+    fontSize: 16,
+    borderColor: darkCream,
+    borderWidth: 2,
+    borderRadius: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    marginTop: 0,
+    marginBottom: 15
+  },
+
+  darkZoneLink: {
+    color: darkGreen,
+    fontFamily: lightFont,
+    fontSize: 14,
+    borderColor: darkGreen,
+    borderBottomWidth: 1,
+    marginBottom: 20
+  },
+
+  modalButton: {
+    backgroundColor: darkCream,
+    color: darkGreen,
+    fontFamily: boldFont,
+    fontSize: 16,
+    borderRadius: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    marginBottom: 15
+  },
+
+  modalButtonRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "65%",
+    marginBottom: -15
+  }
+})
+
+export default styles

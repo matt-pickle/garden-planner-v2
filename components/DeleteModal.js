556676b484418b5650e79bfad8e76f06b81d0f73
Matt <mpickle86@gmail.com>
2024-05-04 20:20:34 -0500

    Add DeleteModal

--- components/DeleteModal.js
+++ components/DeleteModal.js
@@ -1,11 +1,30 @@
-import { useState } from "react"
-import { View, Text } from "react-native"
-// import styles from "../styles/styles.js"
+import { Modal, Text, View, Pressable } from "react-native";
+import styles from "../styles/DeleteModalStyles";
 
-export default function DeleteModal() {
+export default function DeleteModal({ id, gardenName, isDeleteModalVisible, setIsDeleteModalVisible, deleteGarden }) {
+
   return (
-    <View>
-      <Text>DeleteModal</Text>
-    </View>
+    <Modal
+      animationType="slide"
+      visible={isDeleteModalVisible}
+      transparent={true}
+      onRequestClose={() => setIsDeleteModalVisible(false)}
+    >
+      <View style={[styles.modal, styles.centeredModal]}>
+        <Text style={styles.modalText}>Do you want to delete{"\n"}"{gardenName}"?</Text>
+        <View style={styles.modalButtonRow}>
+          <Pressable
+            onPress={() => deleteGarden(id)}
+          >
+            <Text style={[styles.modalButton, styles.yesButton]}>YES</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => setIsDeleteModalVisible(false)}
+          >
+            <Text style={styles.modalButton}>NO</Text>
+          </Pressable>
+        </View>        
+      </View>
+    </Modal>
   )
 }
---
+++ styles/DeleteModalStyles.js
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native"
+import { screenWidth, lightFont, boldFont, cream, darkCream, darkGreen } from "./style-variables.js"
+
+const styles = StyleSheet.create({
+  modal: {
+    flexDirection: "column",
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: cream,
+    borderColor: darkCream,
+    borderWidth: 3,
+    borderRadius: 5,
+    width: screenWidth - 50,
+    padding: 25,
+    marginTop: 25,
+    elevation: 5
+  },
+
+  modalTopRowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%"
+  },
+
+  modalHeader: {
+    color: darkGreen,
+    fontFamily: boldFont,
+    fontSize: 20,
+    marginBottom: 25
+  },
+
+  modalText: {
+    color: darkGreen,
+    fontFamily: lightFont,
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 25
+  },
+
+  modalButton: {
+    backgroundColor: darkCream,
+    color: darkGreen,
+    fontFamily: boldFont,
+    fontSize: 16,
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    marginBottom: 15
+  },
+
+  yesButton: {
+    marginRight: 25
+  },
+
+  modalButtonRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+    marginBottom: -15
+  }
+})
+
+export default styles

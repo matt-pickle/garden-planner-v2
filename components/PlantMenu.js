70c2062beb4609da55d38364e0f3fdd785bb11eb
Matt <mpickle86@gmail.com>
2024-05-06 18:14:16 -0500

    Add PlantMenu

--- components/PlantMenu.js
+++ components/PlantMenu.js
@@ -1,11 +1,53 @@
-import { useState } from "react"
-import { View, Text } from "react-native"
-// import styles from "../styles/styles.js"
+import { useMemo } from "react"
+import { View, ScrollView, Text, Image, Pressable, Dimensions } from "react-native"
+import { useSelectedPlant } from "../context/SelectedPlantContext"
+import plantData from "../database/plantData.js"
+import iconPicker from "../functions/iconPicker"
 
-export default function PlantMenu() {
+export default function PlantMenu({ zone, styles }) {
+  let isMenuHorizontal = true
+  const dim = Dimensions.get("screen")
+  if (dim.width > dim.height) {
+    isMenuHorizontal = false
+  }
+
+  const { selectedPlant, setSelectedPlant } = useSelectedPlant()
+
+  const plantableArr = useMemo(() => {
+    return (
+      plantData.filter(item => {
+        return item.zones.includes(zone)
+      })
+    )
+  }, [])
+  
+  const menu = useMemo(() => {
+    return (
+      plantableArr.map((item, index) => {
+        const icon = iconPicker(item.name);
+        return (
+          <Pressable
+            style={[styles.menuButton, (selectedPlant === item.name && styles.selectedPlantIcon)]}
+            onPress={() => setSelectedPlant(item.name)}
+            key={index}
+          >
+            <Image source={icon} style={{width: "100%", height: "100%"}}/>
+            <Text style={styles.menuButtonText}>{item.name}</Text>
+          </Pressable>
+        );
+      })
+    )
+  }, [plantableArr, selectedPlant])
+  
   return (
-    <View>
-
+    <View style={styles.menuContainer}>
+      <ScrollView
+        horizontal={isMenuHorizontal}
+        contentContainerStyle={styles.contentContainer}
+        persistentScrollbar={true}
+      >
+        {menu}
+      </ScrollView>
     </View>
   )
 }
---
+++ database/plantData.js
@@ -0,0 +1,306 @@
+export default plantData = [
+  {
+    name: "none",
+    zones: ["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b","12a","12b","13a","13b"],
+  },
+
+  {
+    name: "bell pepper",
+    perSquareFoot: 1,
+    zones: ["4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -35,
+    startMessage: "Start bell pepper seeds indoors.",
+    transplantDate: 14,
+    transplantMessage: "Transplant bell peppers to garden (1 per square foot).",
+    harvestDate: 84,
+    harvestMessage: "Harvest bell peppers."
+  },
+  
+  {
+    name: "broccoli",
+    perSquareFoot: 1,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -84,
+    startMessage: "Start broccoli seeds indoors.",
+    transplantDate: -35,
+    transplantMessage: "Transplant broccoli to garden (1 per square foot).",
+    harvestDate: 28,
+    harvestMessage: "Harvest broccoli when head forms, before it starts to bloom."
+  },
+
+  {
+    name: "cabbage",
+    perSquareFoot: 1,
+    zones: ["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -84,
+    startMessage: "Start cabbage seeds indoors.",
+    transplantDate: -35,
+    transplantMessage: "Transplant cabbage to garden (1 per square foot).",
+    harvestDate: 28,
+    harvestMessage: "Harvest cabbage when head is firm."
+  },
+
+  {
+    name: "carrot",
+    perSquareFoot: 16,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -21,
+    startMessage: "Sow carrot seeds in the garden (16 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 49,
+    harvestMessage: "Harvest carrots."
+  },
+
+  {
+    name: "cauliflower",
+    perSquareFoot: 1,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -70,
+    startMessage: "Start cauliflower seeds indoors.",
+    transplantDate: -28,
+    transplantMessage: "Transplant cauliflower to garden (1 per square foot).",
+    harvestDate: 28,
+    harvestMessage: "Harvest cauliflower when head forms, before it starts to bloom."
+  },
+
+  {
+    name: "chard",
+    perSquareFoot: 4,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -49,
+    startMessage: "Start chard seeds indoors.",
+    transplantDate: -21,
+    transplantMessage: "Transplant chard to garden (4 per square foot).",
+    harvestDate: 7,
+    harvestMessage: "Harvest chard, starting with outer leaves when they are about 6 inches tall."
+  },
+
+  {
+    name: "corn",
+    perSquareFoot: 4,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b", "11a", "11b"],
+    startDate: 1,
+    startMessage: "Sow corn seeds in the garden (4 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 63,
+    harvestMessage: "Harvest corn when the silk first browns and the ears feel full."
+  },
+
+  {
+    name: "cucumber",
+    perSquareFoot: 2,
+    zones: ["4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b","12a","12b"],
+    startDate: 7,
+    startMessage: "Sow cucumber seeds in the garden (2 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 70,
+    harvestMessage: "Harvest cucumbers continually, before they turn yellow."
+  },
+
+  {
+    name: "green bean",
+    perSquareFoot: 9,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: 2,
+    startMessage: "Sow green bean seeds in the garden (9 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 58,
+    harvestMessage: "Harvest green beans."
+  },
+
+  {
+    name: "jalapeno",
+    perSquareFoot: 1,
+    zones: ["5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -35,
+    startMessage: "Start jalapeno seeds indoors.",
+    transplantDate: 14,
+    transplantMessage: "Transplant jalapenos to garden (1 per square foot).",
+    harvestDate: 84,
+    harvestMessage: "Harvest jalapenos."
+  },
+
+  {
+    name: "kale",
+    perSquareFoot: 1,
+    zones: ["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -28,
+    startMessage: "Sow kale seeds in the garden (1 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 14,
+    harvestMessage: "Harvest kale when leaves are 6-8 inches long, starting with outer leaves. Can be harvested year-round in zones 7-10."
+  },
+
+  {
+    name: "lettuce",
+    perSquareFoot: 4,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -28,
+    startMessage: "Sow lettuce seeds in the garden (4 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 21,
+    harvestMessage: "Harvest lettuce, starting with outer leaves. Continue harvesting until the plant bolts."
+  },
+
+  {
+    name: "okra",
+    perSquareFoot: 1,
+    zones: ["6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -49,
+    startMessage: "Start okra seeds indoors.",
+    transplantDate: 7,
+    transplantMessage: "Transplant okra to garden (1 per square foot)",
+    harvestDate: 35,
+    harvestMessage: "Harvest okra before pods grow longer than 4 inches."
+  },
+
+  {
+    name: "onion",
+    perSquareFoot: 5,
+    zones: ["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b","12a","12b","13a","13b"],
+    startDate: -70,
+    startMessage: "Start onion seeds indoors.",
+    transplantDate: -28,
+    transplantMessage: "Transplant onions to garden (5 per square foot)",
+    harvestDate: 70,
+    harvestMessage: "Harvest onions when tops turn brown and fall over."
+  },
+
+  {
+    name: "potato",
+    perSquareFoot: 1,
+    zones: ["1a","1b","2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: 0,
+    startMessage: "Plant sprouted potato sections in the garden (1 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 84,
+    harvestMessage: "Harvest potatoes when the tops die."
+  },
+
+  {
+    name: "radish",
+    perSquareFoot: 16,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -21,
+    startMessage: "Sow radish seeds in the garden (16 per square foot). More can be planted throughout the growing season.",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 7,
+    harvestMessage: "Harvest radishes around 4 weeks after planting."
+  },
+
+  {
+    name: "spinach",
+    perSquareFoot: 9,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b"],
+    startDate: -35,
+    startMessage: "Sow spinach seeds in the garden (9 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 14,
+    harvestMessage: "Harvest spinach, starting with outer leaves. Continue harvesting until the plant bolts."
+  },
+
+  {
+    name: "strawberry",
+    perSquareFoot: 4,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -28,
+    startMessage: "Plant strawberry plants in garden (4 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 28,
+    harvestMessage: "Harvest strawberries when ripe. Different varieties will produce at different times. Plants can be overwintered and will produce for a few years."
+  },
+
+  {
+    name: "sugar snap pea",
+    perSquareFoot: 8,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -35,
+    startMessage: "Sow sugar snap pea seeds in the garden (8 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 21,
+    harvestMessage: "Harvest sugar snap peas."
+  },
+
+  {
+    name: "summer squash",
+    perSquareFoot: 1,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b"],
+    startDate: -14,
+    startMessage: "Start summer squash seeds indoors.",
+    transplantDate: 1,
+    transplantMessage: "Transplant summer squash to garden (1 per square foot, more space is better).",
+    harvestDate: 42,
+    harvestMessage: "Harvest summer squash when the fruit is between 6 and 9 inches long."
+  },
+
+  {
+    name: "sweet pea",
+    perSquareFoot: 1,
+    zones: ["3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -35,
+    startMessage: "Sow sweet pea seeds in the garden (1 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 21,
+    harvestMessage: "Harvest sweet peas."
+  },
+
+  {
+    name: "sweet potato",
+    perSquareFoot: 1,
+    zones: ["6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b","12a","12b"],
+    startDate: -35,
+    startMessage: "Start sweet potato sprouts indoors in water. Break sprouts off and plant in soil (indoors) when they reach 3 inches in height.",
+    transplantDate: 21,
+    transplantMessage: "Transplant sweet potatoes to garden (1 per square foot, more space is better).",
+    harvestDate: 141,
+    harvestMessage: "Harvest sweet potatoes before first frost. Cure in warm, humid place for 10 days."
+  },
+
+  {
+    name: "tomato",
+    perSquareFoot: 1,
+    zones: ["4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: -42,
+    startMessage: "Start tomato seeds indoors.",
+    transplantDate: 7,
+    transplantMessage: "Transplant tomatoes to garden after last frost (1 per square foot).",
+    harvestDate: 77,
+    harvestMessage: "Harvest tomatoes as they become ripe."
+  },
+
+  {
+    name: "turnip",
+    perSquareFoot: 9,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b"],
+    startDate: -14,
+    startMessage: "Sow turnip seeds in the garden (9 per square foot).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 35,
+    harvestMessage: "Harvest turnips."
+  },
+
+  {
+    name: "winter squash",
+    perSquareFoot: 1,
+    zones: ["2a","2b","3a","3b","4a","4b","5a","5b","6a","6b","7a","7b","8a","8b","9a","9b","10a","10b","11a","11b"],
+    startDate: 14,
+    startMessage: "Sow winter squash seeds in the garden (1 per square foot, more space is better).",
+    transplantDate: null,
+    transplantMessage: null,
+    harvestDate: 98,
+    harvestMessage: "Harvest winter squash before the first hard frost."
+  }
+];

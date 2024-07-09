97d2ce3376f46a6de60b0bd9d393655df42a6574
Matt <mpickle86@gmail.com>
2024-07-08 21:36:57 -0500

    Remove admob ads for web version

---
+++ components/DashTopBar.web.js
@@ -0,0 +1,51 @@
+import { View, Text, Pressable } from "react-native"
+import { Entypo } from '@expo/vector-icons'
+import styles from "../styles/DashTopBarStyles"
+
+export default function DashTopBar({ isScheduleOpen, setIsScheduleOpen, setIsSettingsVisible }) {    
+
+  async function openSchedule() {
+    setIsScheduleOpen(true)
+  }
+
+  async function closeSchedule() {
+    setIsScheduleOpen(false)
+  }
+
+  return (
+    <View style={styles.dashTopBar}>
+      <View style={styles.dashTopBarInner}>
+        <View style={styles.dashTabContainer}>
+          <Pressable
+            style={[styles.dashTabBtn, (!isScheduleOpen && styles.selectedTab)]}
+            onPress={closeSchedule}
+          >
+            <Text
+              style={[styles.tabText, (!isScheduleOpen && styles.selectedTab)]}
+            >
+              GARDENS
+            </Text>
+          </Pressable>
+          <Pressable
+            style={[styles.dashTabBtn, (isScheduleOpen && styles.selectedTab)]}
+            onPress={openSchedule}
+          >
+            <Text
+              style={[styles.tabText, (isScheduleOpen && styles.selectedTab)]}
+            >
+              SCHEDULE
+            </Text>
+          </Pressable>
+        </View>
+        <Pressable
+          onPress={() => setIsSettingsVisible(true)}
+        >
+          <Entypo
+            name="dots-three-vertical"
+            style={styles.dashIcon}
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
---
+++ components/Dashboard.web.js
@@ -0,0 +1,179 @@
+import { useState, useEffect } from "react"
+import { View } from "react-native"
+import { logOut } from "../firebase/firebase-auth-methods"
+import { updateUserData, deleteAccount } from "../firebase/firebase"
+import SettingsModal from "./SettingsModal"
+import ZoneModal from "./ZoneModal"
+import CreateGardenModal from "./CreateGardenModal"
+import DeleteModal from "./DeleteModal"
+import DashTopBar from "./DashTopBar"
+import GardenList from "./GardenList"
+import GardenEditor from "./GardenEditor"
+import Schedule from "./Schedule"
+import styles from "../styles/DashboardStyles"
+
+export default function Dashboard({ user, userData, setScreen, orientation }) {
+  const [zone, setZone] = useState(userData.zone)
+  const [gardens, setGardens] = useState(userData.gardens)
+  const [isZoneModalVisible, setIsZoneModalVisible] = useState(false)
+  const [isSettingsVisible, setIsSettingsVisible] = useState(false)
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false)
+  const [isCreateGardenModalVisible, setIsCreateGardenModalVisible] = useState(false)
+  const [isEditorOpen, setIsEditorOpen] = useState(false)
+  const [isScheduleOpen, setIsScheduleOpen] = useState(false)
+  const [gardenToDelete, setGardenToDelete] = useState({})
+  const [displayedGarden, setDisplayedGarden] = useState(null)
+
+  useEffect(() => {
+    if (!zone) {
+      setIsZoneModalVisible(true)
+    }
+  },[])
+
+  function changeZone(newZone) {
+    setZone(newZone)
+    updateUserData(user.uid, { zone: newZone })
+  }
+
+  function createNewGarden(gardenName) {
+    const updatedGardensArr = [...gardens, {
+      id: Date.now(),
+      gardenName: gardenName,
+      width: 4,
+      height: 4,
+      plantedArr: []
+    }]
+    const updatedUserData = {
+      gardens: updatedGardensArr
+    }
+    setGardens(updatedGardensArr)
+    updateUserData(user.uid, updatedUserData)
+    setIsCreateGardenModalVisible(false)
+  }
+
+  async function openEditor(garden) {
+    setDisplayedGarden(garden)
+    setIsEditorOpen(true)
+  }
+
+  function saveGarden(width, height, plantedArr) {
+    const updatedGardensArr = gardens.filter(item => {
+      return item.id !== displayedGarden.id
+    })   
+    updatedGardensArr.push({
+      id: displayedGarden.id,
+      gardenName: displayedGarden.gardenName,
+      width: width,
+      height: height,
+      plantedArr: plantedArr
+    })
+    setGardens(updatedGardensArr)
+    const updatedUserData = {
+      gardens: updatedGardensArr
+    }
+    updateUserData(user.uid, updatedUserData)
+  }
+
+  function saveAndClose(width, height, plantedArr) {
+    saveGarden(width, height, plantedArr)
+    setIsEditorOpen(false)
+    setDisplayedGarden(null)
+  }
+
+  function openDeleteModal(garden) {
+    setGardenToDelete(garden)
+    setIsDeleteModalVisible(true)
+  }
+
+  function deleteGarden(id) {
+    const updatedGardensArr = gardens.filter(item => {
+      return item.id !== id
+    })
+    setGardens(updatedGardensArr)
+    const updatedUserData = {
+      gardens: updatedGardensArr
+    }
+    updateUserData(user.uid, updatedUserData)
+    setIsEditorOpen(false)
+    setDisplayedGarden(null)
+    setIsDeleteModalVisible(false)
+  }
+
+  function handleLogOut() {
+    logOut()
+    setScreen("Login")
+  }
+
+  function handleDeleteAccount() {
+    deleteAccount(user.uid)
+    handleLogOut()
+  }
+
+  const dashTopBar = <DashTopBar
+    isScheduleOpen={isScheduleOpen}
+    setIsScheduleOpen={setIsScheduleOpen}
+    setIsSettingsVisible={setIsSettingsVisible}
+  />
+
+  const gardenList = <GardenList 
+    gardens={gardens}
+    openEditor={openEditor}
+    setIsCreateGardenModalVisible={setIsCreateGardenModalVisible}
+    openDeleteModal={openDeleteModal}
+  />
+
+  const gardenEditor = <GardenEditor
+    displayedGarden={displayedGarden}
+    zone={zone}
+    saveAndClose={saveAndClose}
+    openDeleteModal={openDeleteModal}
+    orientation={orientation}
+  />
+
+  const schedule = <Schedule
+    gardens={gardens}
+    zone={zone}
+  />
+
+  return (
+    <View style={styles.dashContainer}>
+      {!isEditorOpen && dashTopBar}
+      <ZoneModal
+        isZoneModalVisible={isZoneModalVisible}
+        setIsZoneModalVisible={setIsZoneModalVisible}
+        zone={zone}
+        changeZone={changeZone}
+      />
+      <SettingsModal
+        isSettingsVisible={isSettingsVisible}
+        setIsSettingsVisible={setIsSettingsVisible}
+        name={user.displayName}
+        zone={zone}
+        changeZone={changeZone}
+        handleLogOut={handleLogOut}
+        handleDeleteAccount={handleDeleteAccount}
+      />
+      <CreateGardenModal 
+        isCreateGardenModalVisible={isCreateGardenModalVisible}
+        setIsCreateGardenModalVisible={setIsCreateGardenModalVisible}
+        createNewGarden={createNewGarden}
+      />
+      <DeleteModal
+        isDeleteModalVisible={isDeleteModalVisible}
+        setIsDeleteModalVisible={setIsDeleteModalVisible}
+        id={gardenToDelete.id}
+        gardenName={gardenToDelete.gardenName}
+        deleteGarden={deleteGarden}
+      />
+      {
+        gardens && !isEditorOpen && !isScheduleOpen ?
+        gardenList : 
+        gardens && isEditorOpen ?
+        gardenEditor :
+        gardens && isScheduleOpen ?
+        schedule :
+        null
+      }
+    </View>
+  )
+}
